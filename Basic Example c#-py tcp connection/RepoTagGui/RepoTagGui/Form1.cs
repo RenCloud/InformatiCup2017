722f36b43fd2f234b9a084858fdefa6d1cc3eef3
RenCloud <thedarkrencloud@yandex.ru>
2016-12-17 19:47:07 +0100

    Added a c#<->py tcp example
    c# gui with button and ip/port textfields to send a "string" to server
    python server recieved string and print it to shell

---
+++ Basic Example c#-py tcp connection/RepoTagGui/RepoTagGui/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RepoTagGui {
+    public partial class Form1 : Form {
+        private Stream inOUt;
+        private TcpClient tcpClient;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void b_createTCPConnection_Click(object sender, EventArgs e) {
+            b_createTCPConnection.Enabled =false ;
+            tcpClient = new TcpClient(tb_ip.Text, int.Parse(tb_port.Text));
+            inOUt = tcpClient.GetStream();
+            var sendBytes = Encoding.ASCII.GetBytes("TestString to Start Training on Python Server really long...");
+            inOUt.Write(sendBytes, 0, sendBytes.Length);
+            tcpClient.Close();
+            b_createTCPConnection.Enabled =true ;
+        }
+    }
+}
