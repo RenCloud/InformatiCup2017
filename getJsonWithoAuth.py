dab5a274a432dee01fb7bdba5d716ad2f6999533
RenCloud <thedarkrencloud@yandex.ru>
2017-01-14 16:20:33 +0100

    added functionality so it can get up to 1000 commits comments issues

--- getJsonWithoAuth.py
+++ getJsonWithoAuth.py
@@ -46,21 +46,110 @@ def getJson2(url, github):
 
     # Request Commits Data ( up to 100 commits)
     r = github.get('https://api.github.com/repos/' + owner + '/' + repos + '/commits?per_page=100')
+
     if r.ok:
         # CommitJson -> Object
-        repoItemCommits = json.loads(r.text or r.content)
+
+        try:
+            headers = r.headers['link']
+            test = headers.split(',')
+            startIndex = test[1].find('&page=')
+            end = test[1][startIndex+6:startIndex+6+2]
+            if end[1] == '>':
+                end = end[0]
+
+            if int(end)<10:
+                repoItemCommits = json.loads(r.text or r.content)
+                for i in range(2, int(end)+1):
+                    r = github.get(
+                        'https://api.github.com/repos/' + owner + '/' + repos + '/commits?per_page=100&page=' + str(i))
+                    repoItemCommits += json.loads(r.text or r.content)
+
+
+
+            else:
+                repoItemCommits = json.loads(r.text or r.content)
+                for i in range(2, 11):
+                    r = github.get('https://api.github.com/repos/' + owner + '/' + repos + '/commits?per_page=100&page=' + str(i))
+                    repoItemCommits += json.loads(r.text or r.content)
+
+
+
+
+
+        except:
+            repoItemCommits = json.loads(r.text or r.content)
+
 
     # Request Comments Data ( up to 100)
     r = github.get('https://api.github.com/repos/' + owner + '/' + repos + '/comments?per_page=100')
     if r.ok:
-        # CommentJson -> Object
-        repoItemsComment = json.loads(r.text or r.context)
+
+        try:
+            headers = r.headers['link']
+            test = headers.split(',')
+            startIndex = test[1].find('&page=')
+            end = test[1][startIndex + 6:startIndex + 6 + 2]
+            if end[1] == '>':
+                end = end[0]
+
+            if int(end) < 10:
+                repoItemsComment = json.loads(r.text or r.content)
+                for i in range(2, int(end) + 1):
+                    r = github.get(
+                        'https://api.github.com/repos/' + owner + '/' + repos + '/comments?per_page=100&page=' + str(i))
+                    repoItemsComment += json.loads(r.text or r.content)
+
+
+
+            else:
+                repoItemsComment = json.loads(r.text or r.content)
+                for i in range(2, 11):
+                    r = github.get(
+                        'https://api.github.com/repos/' + owner + '/' + repos + '/comments?per_page=100&page=' + str(i))
+                    repoItemsComment += json.loads(r.text or r.content)
+
+
+
+
+
+        except:
+            repoItemsComment = json.loads(r.text or r.content)
 
     # Request Ussue Data ( Up to 100)
     r = github.get('https://api.github.com/repos/' + owner + '/' + repos + '/issues?per_page=100')
     if r.ok:
-        # IssueJson -> Object
-        repoItemIssue = json.loads(r.text or r.context)
+
+        try:
+            headers = r.headers['link']
+            test = headers.split(',')
+            startIndex = test[1].find('&page=')
+            end = test[1][startIndex + 6:startIndex + 6 + 2]
+            if end[1] == '>':
+                end = end[0]
+
+            if int(end) < 10:
+                repoItemIssue = json.loads(r.text or r.content)
+                for i in range(2, int(end) + 1):
+                    r = github.get(
+                        'https://api.github.com/repos/' + owner + '/' + repos + '/issues?per_page=100&page=' + str(i))
+                    repoItemIssue += json.loads(r.text or r.content)
+
+
+
+            else:
+                repoItemIssue = json.loads(r.text or r.content)
+                for i in range(2, 11):
+                    r = github.get(
+                        'https://api.github.com/repos/' + owner + '/' + repos + '/issues?per_page=100&page=' + str(i))
+                    repoItemIssue += json.loads(r.text or r.content)
+
+
+
+
+
+        except:
+            repoItemIssue = json.loads(r.text or r.content)
     # Request Repo Infos
     r = github.get('https://api.github.com/repos/' + owner + '/' + repos)
     if r.ok:
