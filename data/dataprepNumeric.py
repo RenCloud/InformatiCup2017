28cb889e4903d00e1893eba04894069bac3f8a90
Jan Feuchter <j.feuchter@t-online.de>
2017-01-19 15:06:45 +0100

    added docs

--- data/dataprepNumeric.py
+++ data/dataprepNumeric.py
@@ -1,7 +1,5 @@
 import json
-from collections import defaultdict
 import numpy
-import gensim
 import os.path
 import math
 
@@ -34,9 +32,10 @@ def prep(file, training = 0):
         for path in repo['repository']:
             if(path['type'] == "Blob"):
                 files+=1
-        vec.append(logisFile(files))
-        vec.append(logis(len(repo['commits'])/files)) #relativ zu Anzahl Ordner+Files
-        vec.append(logis(len(repo['comments'])/len(repo['commits']))) #relativ zu commits
+        #all values are normalized with a logistic function
+        vec.append(logisFile(files)) #number of files normalized
+        vec.append(logis(len(repo['commits'])/files)) #commits per file ratio normalized
+        vec.append(logis(len(repo['comments'])/len(repo['commits']))) #average comments per commit normalized
         openI = 0
         closedI = 1
         for i in range(len(repo['issue'])):
@@ -44,37 +43,18 @@ def prep(file, training = 0):
                 openI += 1
             elif(repo['issue'][i]['state'] == 'closed'):
                 closedI += 1
-        vec.append(logis(openI/closedI)) #nur open-closed
+        vec.append(logis(openI/closedI)) #open-closed issue ratio normalized
         author = []
         for commit in repo['commits']:
             if(commit['author_login'] not in author):
                 author.append(commit['author_login'])
-        vec.append(logis(len(author)))
+        vec.append(logis(len(author))) #number of unique authors normalized
         committer = []
         for commit in repo['commits']:
             if((commit['committer_login'] not in committer)):
                 committer.append(commit['committer_login'])
-        vec.append(logis(len(committer)))
+        vec.append(logis(len(committer))) #number of unique commiters normalized
 
-        #ordnernamen
-
-        """dic_end = defaultdict(int)
-        sum = 0;
-        endings = []
-        for path in repo:
-            if(path['type'] == "Blob"):
-                parts = path['path'].split(".")
-                for part in parts:
-                    last = part
-                dic_end[last] += 1
-                sum+=1;
-        for e in list(dic_end):
-            if(dic_end[e] <= sum/100):
-                del(dic_end[e])
-            else:
-                for i in range (0, dic_end[e]):
-                    endings.append(e)
-        return endings"""
         json_out_raw_arr.append(vec)
 
     return json_out_raw_arr
